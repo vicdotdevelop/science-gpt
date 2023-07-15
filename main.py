18ca23bbef3f90ba0fc6527dcda4e6da7cffe811
Victor <60963842+vicdotdevelop@users.noreply.github.com>
2023-07-15 14:27:39 +0200

    refactored code

--- main.py
+++ main.py
@@ -14,45 +14,64 @@ load_dotenv()
 # Set the OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def read_pdf(file):
+def read_pdf(pdf_file):
+    """
+    Reads the text from a PDF file and generates a citation.
+
+    Args:
+        pdf_file: The PDF file to read.
+
+    Returns:
+        A tuple containing the extracted text and the generated citation.
+    """
     # Initialize a PDF file reader object
-    pdf_reader = PdfReader(file)
+    pdf_reader = PdfReader(pdf_file)
     
     # Initialize an empty string to hold the extracted text
-    text = ""
+    extracted_text = ""
     
     # Loop through each page in the PDF file and extract the text
     for page in pdf_reader.pages:
-        text += page.extract_text()
+        extracted_text += page.extract_text()
     
     # Generate a citation for the PDF file
-    citation = generate_citation(text)
+    citation = generate_citation(extracted_text)
     
-    return text, citation
+    return extracted_text, citation
 
 def generate_citation(text):
     # For now, return a placeholder citation
     citation = "Placeholder citation for: " + text[:50] + "..."
     return citation
 
-def send_request_to_gpt4(text, citation):
+def send_request_to_gpt4(input_text, citation):
+    """
+    Sends a request to the GPT-4 model with the input text and citation.
+
+    Args:
+        input_text: The text to send to the GPT-4 model.
+        citation: The citation to include in the response.
+
+    Returns:
+        The response from the GPT-4 model.
+    """
     # Load the BERT model
     tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-    # Tokenize the text and split it into chunks of 4096 tokens
-    tokens = tokenizer.tokenize(text)
-    chunks = [tokens[i:i + 4096] for i in range(0, len(tokens), 4096)]
+    # Tokenize the input text and split it into chunks of 4096 tokens
+    tokens = tokenizer.tokenize(input_text)
+    token_chunks = [tokens[i:i + 4096] for i in range(0, len(tokens), 4096)]
 
-    responses = []
+    gpt_responses = []
 
     # Send a request to the GPT-4 model with each chunk
-    for chunk in chunks:
+    for chunk in token_chunks:
         chunk_text = tokenizer.convert_tokens_to_string(chunk)
         for i in range(5):  # Retry up to 5 times
             try:
                 response = openai.ChatCompletion.create(model="gpt-4", messages=[{"role": "system", "content": scientific_writing_specialist}, {"role": "user", "content": chunk_text}])
-                responses.append(response['choices'][0]['message']['content'])  # type: ignore
-                responses.append(citation)  # Add the citation to the response
+                gpt_responses.append(response['choices'][0]['message']['content'])  # type: ignore
+                gpt_responses.append(citation)  # Add the citation to the response
                 time.sleep(0.1)  # Add a delay between each request to avoid hitting the rate limit
                 break
             except openai.error.RateLimitError as e:  # Catch the RateLimitError # type: ignore
@@ -61,7 +80,7 @@ def send_request_to_gpt4(text, citation):
                 else:
                     raise e  # If the last retry attempt, re-raise the exception
 
-    return " ".join(responses)
+    return " ".join(gpt_responses)
 def main():
     files = st.file_uploader("Upload PDF Files", type="pdf", accept_multiple_files=True)
     
